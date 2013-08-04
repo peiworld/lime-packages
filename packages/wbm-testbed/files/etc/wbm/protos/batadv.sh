137d4278e0c780dee456c7c6fb68b2bd24e76cb8
Gui Iribarren <gui@altermundi.net>
2013-08-04 19:44:24 -0300

    half-baked cleanup of wbmv6 code and first adaptations. doesn't run cleanly yet

--- packages/wbm-testbed/files/etc/wbm/protos/batadv.sh
+++ packages/wbm-testbed/files/etc/wbm/protos/batadv.sh
@@ -8,26 +8,18 @@ IPV6=$5
 R1=$6
 R2=$7
 
-ipv4_addr () {
-  echo ${IPV4%%/*}
-}
-
-ipv4_netmask () {
-  echo ${IPV4##*/}
-}
-
 clean () {
   true  
 }
 
 prepare () {
-  uci set batman-adv.bat0=mesh
-  uci set batman-adv.bat0.bridge_loop_avoidance=1
-  uci commit batman-adv
+#  uci set batman-adv.bat0=mesh
+#  uci set batman-adv.bat0.bridge_loop_avoidance=1
+#  uci commit batman-adv
 
   uci set network.bat0=interface
   uci set network.bat0.ifname=bat0
-  uci set network.bat0.proto=static
+  uci set network.bat0.proto=batmesh
   uci set network.bat0.ip6addr=""
   uci set network.bat0.ipaddr=""
   uci set network.bat0.netmask=""
@@ -36,16 +28,6 @@ prepare () {
 }
 
 add () {
-  if [ "$(uci -q get network.bat0.macaddr)" == "" ] ; then
-    uci set network.bat0.macaddr="$(printf '02:ba:ff:%02x:%02x:01' $R1 $R2)"
-  fi
-  if [ "$(uci -q get network.bat0.ip6addr)" == "" ] ; then
-    uci set network.bat0.ip6addr="$(printf 'fdbb::%02x%02x/64' $R1 $R2)"
-  fi
-  if [ "$(uci -q get network.bat0.ipaddr)" == "" ] ; then
-    uci set network.bat0.ipaddr="$(ipv4_addr)"
-    uci set network.bat0.netmask="$(ipv4_netmask)"
-  fi
   uci set network.${LOGICAL_INTERFACE}=interface
   uci set network.${LOGICAL_INTERFACE}.proto=batadv
   uci set network.${LOGICAL_INTERFACE}.mesh=bat0
